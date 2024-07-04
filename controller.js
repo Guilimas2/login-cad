f6b9741ceb38eb9ddd8132d98bec0e830f62d6b6
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-07-04 11:41:22 -0300

    terminamos o javascript do login, começamos o javascript do cadastro

--- controller.js
+++ controller.js
@@ -1,12 +1,17 @@
 // validaçao para validaçao de acessar 
- function acessar(){
-    let loginEmail = document.getElementById ('loginEmail').value; 
-    let loginsenha =  document.getElementById ('loginSenha'). value; 
-
-    if(!loginEmail || !loginsenha){
-        alert("Por favor preencher todos os campos");
-    }else{
-        // alert("Campos preenchidos com sucesso");
-        window.location.href = 'cadastro.html';
+function acessar() {
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+ 
+    if (!loginEmail || !loginSenha) {
+        alert("Favor preencher todos os campos!");
+    } else {
+        // alert("Campos preenchidos com sucesso!");
+        window.location.href = "cadastro.html";
     }
+}
+// FUNÇAO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+var dadosLista = [];
+function salvarUser(){
+
 }
