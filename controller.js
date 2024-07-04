09f9754dcbc8be14be7556567d24a5f381fa1e5b
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-07-04 11:04:06 -0300

    iniciando os javascript e colocamos o acessar no botao

---
+++ controller.js
@@ -0,0 +1,11 @@
+// validaçao para validaçao de acessar 
+ function acessar(){
+    let loginEmail = document.getElementById ('loginEmail').value; 
+    let loginsenha =  document.getElementById ('loginSenha'). value; 
+
+    if (!loginEmail || !loginsenha){
+        alert("Por favor preencher todos os campos");
+    }else{
+        alert("Campos preenchidos com sucesso"); 
+    }
+ }
