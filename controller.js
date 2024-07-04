d9b850ee4981abf1d0b31f97ced66d544abd80e3
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-07-04 11:11:43 -0300

    quando voce apertar em acessar ira para outro site

--- controller.js
+++ controller.js
@@ -3,9 +3,10 @@
     let loginEmail = document.getElementById ('loginEmail').value; 
     let loginsenha =  document.getElementById ('loginSenha'). value; 
 
-    if (!loginEmail || !loginsenha){
+    if(!loginEmail || !loginsenha){
         alert("Por favor preencher todos os campos");
     }else{
-        alert("Campos preenchidos com sucesso"); 
+        // alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html';
     }
- }
+}
