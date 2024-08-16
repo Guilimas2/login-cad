5b808c05c9e14465e0e983d3bd8be4c32d19aa87
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-08-16 09:29:38 -0300

    coloquei a funçao do email e campos do email

--- controller.js
+++ controller.js
@@ -11,23 +11,26 @@ function acessar(){
 // SE TIVER UM CAMPO VAZIO IRA APARECER UMA MENSAGEM DE ALERTA SOLICITANDO PARA PREENCHER TODOS OS CAMPOS 
     }else{
         //alert("Campos preenchidos com sucesso");
-        window.location.href = 'cadatsro.html';
+        window.location.href = 'cadastro.html';
     }
 }
  
 // FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
 var dadosLista = [];
+var email = [];
 //  CRIAR UM ARRAY VAZIO PARA ARMAZENAR OS NOMES  DOS USUARIO
 function salvarUser(){
     // O 'salvarUser' RESPONSALVEL POR ADICIONAR UM NOVO NOME AO ARRAY 
     let nomeUser = document.getElementById('nomeUser').value;
-    // OBTER O VALOR INSERIDO PELO USUARIONO CAMPO DO NOME 
+    let emailUser = document.getElementById('emailUser').value;
     if(nomeUser){
         dadosLista.push(nomeUser);
         //console.log(dadosLista);
+        email.push(emailUser);
         criaLista();
         // LIMPA O CAMPOS DE ENTRADA DE NOME APOS O SALVAMENTO 
         document.getElementById('nomeUser').value = "";
+        document.getElementById('emailUser').value = "";
     }else{
         alert("Favor informe um nome para cadastro");
         // SEO CAMPO DE NOME ESTIVER VAZIO, IRA APARECER UMA MENSAGEM DE ALERTA PARA PREENCHER O NOME SEJA INFORMADO 
@@ -36,12 +39,11 @@ function salvarUser(){
 // FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
 function criaLista(){
     // INICIAR A TABELA COM O CABEÇALHO 
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
     // O ARRAY "dadosLista" PARA ADICIONAR CADA NOME A TABELA
     for(let i=0;i<=(dadosLista.length - 1);i++){
         // ADICIONA UMA NOVA LINHA PARA CADA NOME NA TABELA
-
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + email[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         // ATUALIZA O CONTEUDO DA TABELA NO HTML COM O CODIGO DA TABELA GERADO
         document.getElementById('tabela').innerHTML = tabela;
         // FUÇAO PARA EDITAR NOMES DE LISTA 
@@ -52,7 +54,9 @@ function criaLista(){
 function editar(i){
 // PREENCHER O CAMPO DE ENTRADA COM O NOME A SER EDITADO 
     document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    document.getElementById('emailUser').value = email[(i - 1)];
     dadosLista.splice(dadosLista[(i - 1)], 1);
+    email.splice((i - 1), 1);
     // REMOVE O NOME DA LISTA PARA QUE POSSA SER ATUALIZADO 
 
 }
@@ -61,6 +65,8 @@ function editar(i){
 function excluir(i){
     // REMOVE  O NOME DA LISTA PELO INDICE FORNECIDO
     dadosLista.splice((i - 1), 1);
+    email.splice((i - 1), 1);
     // ATUALIZA A TABELA APOS A REMOÇAO
     document.getElementById('tabela').deleteRow(i);
+    document.getElementById('nomeUser').value= "";
 }
