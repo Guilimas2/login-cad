f8f9c6569d1a206d3db96c51634c09145f5d5c46
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-08-30 07:56:42 -0300

    fiz os campos de cpf e fiz e estou fazendo a validacao dos campo do cpf no javascript

--- controller.js
+++ controller.js
@@ -1,72 +1,79 @@
-// FUNÇÃO PARA VALIDAÇÃO DE ACESSO
-
-//  A FUNÇAO ACESSAR E PARA VALIDAR OS CAMPOS LOGIN
-function acessar(){
-    // OBTER O VALOR INSERIDO PELO USUARIO NOS CAMPOS DE EAMIL E SENHA
-    let loginEmail = document.getElementById('loginEmail').value;
-    let loginSenha = document.getElementById('loginSenha').value;
-//  PARA VER SE TEM ALGUM CAMPO PREENCHIDO 
-    if(!loginEmail || !loginSenha){
+// Função para validar o acesso na tela de login
+function acessar() {
+    // Obtém os valores dos campos de email e senha
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+    // Verifica se os campos estão preenchidos
+    if (!loginEmail || !loginSenha) {
+        // Se algum campo estiver vazio, exibe um alerta
         alert("Favor preencher todos os campos");
-// SE TIVER UM CAMPO VAZIO IRA APARECER UMA MENSAGEM DE ALERTA SOLICITANDO PARA PREENCHER TODOS OS CAMPOS 
-    }else{
-        //alert("Campos preenchidos com sucesso");
-        window.location.href = 'cadastro.html';
+    } else {
+        // Se ambos os campos estiverem preenchidos, redireciona para a página de cadastro
+        window.location.href = "cadastro.html";
     }
 }
- 
-// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+// Array que armazena a lista de nomes de usuários
 var dadosLista = [];
-var email = [];
-//  CRIAR UM ARRAY VAZIO PARA ARMAZENAR OS NOMES  DOS USUARIO
-function salvarUser(){
-    // O 'salvarUser' RESPONSALVEL POR ADICIONAR UM NOVO NOME AO ARRAY 
-    let nomeUser = document.getElementById('nomeUser').value;
-    let emailUser = document.getElementById('emailUser').value;
-    if(nomeUser){
-        dadosLista.push(nomeUser);
-        //console.log(dadosLista);
-        email.push(emailUser);
-        criaLista();
-        // LIMPA O CAMPOS DE ENTRADA DE NOME APOS O SALVAMENTO 
-        document.getElementById('nomeUser').value = "";
-        document.getElementById('emailUser').value = "";
-    }else{
-        alert("Favor informe um nome para cadastro");
-        // SEO CAMPO DE NOME ESTIVER VAZIO, IRA APARECER UMA MENSAGEM DE ALERTA PARA PREENCHER O NOME SEJA INFORMADO 
-    }
-}
-// FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
-function criaLista(){
-    // INICIAR A TABELA COM O CABEÇALHO 
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
-    // O ARRAY "dadosLista" PARA ADICIONAR CADA NOME A TABELA
-    for(let i=0;i<=(dadosLista.length - 1);i++){
-        // ADICIONA UMA NOVA LINHA PARA CADA NOME NA TABELA
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + email[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
-        // ATUALIZA O CONTEUDO DA TABELA NO HTML COM O CODIGO DA TABELA GERADO
-        document.getElementById('tabela').innerHTML = tabela;
-        // FUÇAO PARA EDITAR NOMES DE LISTA 
-    }
-}
- 
-// FUNÇÃO PARA EDITAR NOMES DE LISTA
-function editar(i){
-// PREENCHER O CAMPO DE ENTRADA COM O NOME A SER EDITADO 
-    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-    document.getElementById('emailUser').value = email[(i - 1)];
-    dadosLista.splice(dadosLista[(i - 1)], 1);
-    email.splice((i - 1), 1);
-    // REMOVE O NOME DA LISTA PARA QUE POSSA SER ATUALIZADO 
+var EmailLista = [];
+var CPFlista = [];
+// Função para salvar o nome do usuário na lista
+function salvarUser() {
+    // Obtém o valor do campo de nome de usuário
+    let nomeUser = document.getElementById("nomeUser").value;
+    let emailUser = document.getElementById("emailUser").value;
+    let CPFUser = document.getElementById("CPFUser").value;
 
+    // Verifica se o campo de nome não está vazio
+    if (document.getElementById('emailUser').value == "" || document.getElementById('emailUser').value.indexOf('@') == -1 || document.getElementById('emailUser').value.indexOf('.') == -1) {
+        // Se alguma das condições acima for verdadeira, exibe um alerta
+        alert("Por favor, informe um e-mail válido");
+    } else if (nomeUser && emailUser && CPFUser) {
+        // Adiciona o nome ao array de dadosLista
+        dadosLista.push(nomeUser);
+        EmailLista.push(emailUser);
+        CPFlista.push(CPFUser);
+        // Atualiza a lista exibida na tela
+        criarLista();
+        // Limpa o campo de nome de usuário
+        document.getElementById("nomeUser").value = "";
+        document.getElementById("emailUser").value = "";
+        document.getElementById("CPFUser").value = "";
+    } else {
+        // Se o campo estiver vazio, exibe um alerta
+        alert("Favor informa todos os campos para cadastrado");
+    }
 }
- 
-// FUNÇÃO PARA EXCLUIR NOME DE LISTA
-function excluir(i){
-    // REMOVE  O NOME DA LISTA PELO INDICE FORNECIDO
+
+// Função para criar e atualizar a lista de usuários na tela
+function criarLista() {
+    // Cria o cabeçalho da tabela
+    let tabela = "<tr><th> Nome Usuário</th><th>E-mail</th><th>CPF</t><th>Ações</th>";
+
+    // Itera sobre todos os nomes na lista de dados
+    for (let i = 0; i <= (dadosLista.length - 1); i++) {
+        // Adiciona uma nova linha para cada nome com um botão de editar
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + EmailLista[i] + "</td><td>" + CPFlista[i] + "</td><td><button type='button' onclick='editar(this.parentNode.parentNode.rowIndex)' class='btn-rosa'>Editar</button><button type='button' onclick='excluir(this.parentNode.parentNode.rowIndex)' class='btn-rosa'>excluir</button></td></tr>";
+    }
+
+    // Atualiza o conteúdo da tabela com a nova lista
+    document.getElementById("tabela").innerHTML = tabela;
+}
+
+// Função para editar um nome na lista
+function editar(i) {
+    // Preenche o campo de nome com o nome selecionado para edição
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    document.getElementById('emailUser').value = EmailLista[(i - 1)];
+    document.getElementById('CPFUser').value = CPFlista[(i - 1)];
+    // seleciona para a edição
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+    EmailLista.splice(EmailLista[(i - 1)], 1);
+    CPFlista.splice(CPFlista[(i - 1)], 1);
+    // Atualiza a lista exibida na tela após a remoção
+}
+// Função para excluir um nome na lista
+function excluir(i) {
     dadosLista.splice((i - 1), 1);
-    email.splice((i - 1), 1);
-    // ATUALIZA A TABELA APOS A REMOÇAO
-    document.getElementById('tabela').deleteRow(i);
-    document.getElementById('nomeUser').value= "";
+    document.getElementById("tabela").deleteRow(i);
 }
+
